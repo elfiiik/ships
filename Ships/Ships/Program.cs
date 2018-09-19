1bc2bdad965653de040df422c38611e62ae5a220
punaja16 <punaja16@DST20705>
2018-09-19 16:06:22 +0200

    init

--- Ships/Ships/Program.cs
+++ Ships/Ships/Program.cs
@@ -10,6 +10,49 @@ namespace Ships
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("napiste souradnici x");
+            int playerx = Console.Read();
+            Console.WriteLine("napiste souradnici y");
+            int playery = Console.Read();
+
+            List<Point> pole = new List<Point>();
+            bool radek1;
+            for (int i = 0; i < 10; i++)
+            {
+                
+                for (int j = 0; j < 10; j++)
+                {
+                    if (j == 9)
+                    {
+                        radek1 = true;
+                    }
+                    else
+                    {
+                        radek1 = false;
+                    }
+                    pole.Add(new Point
+                    {
+                        x = i,
+                        y = j,
+                        radek = radek1
+                    });
+                }
+            }
+
+            foreach (Point point in pole)
+            {
+                //Console.WriteLine($"X={point.x} a Y={point.y}-----{point.radek}");
+                Console.Write("*");
+                if (point.radek)
+                {
+                    Console.WriteLine();
+                }
+            }
+
+            List<Ship> ships = new List<Ship>();
+            
+
         }
     }
 }
---
+++ Ships/Ships/Ship.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ships
+{
+    class Ship
+    {
+
+        private int x;
+        private int y;
+        private string type;
+
+    }
+}
